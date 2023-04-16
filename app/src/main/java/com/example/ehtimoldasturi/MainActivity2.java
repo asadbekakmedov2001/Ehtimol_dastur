cfa342288fa7692f8f62ea91041d8080f2660d12
Asadbek Axmedov <100250308+asadbekakmedov2001@users.noreply.github.com>
2023-04-16 09:45:50 +0500

    a

--- app/src/main/java/com/example/ehtimoldasturi/MainActivity2.java
+++ app/src/main/java/com/example/ehtimoldasturi/MainActivity2.java
@@ -66,34 +66,34 @@ public class MainActivity2 extends AppCompatActivity implements UserAdapterAC2.S
 
                 textViewSelectedChapterName.setText("Tеоретиечская часть");
 
-                modelRcyclers.add(new ModelRcycler("Тема 1. Основные понятия и определения.",""));
-                modelRcyclers.add(new ModelRcycler("Тема 2. События и операции над ними.",""));
-                modelRcyclers.add(new ModelRcycler("Тема 3. Понятие вероятности события, его классическое и статистическое определения.",""));
-                modelRcyclers.add(new ModelRcycler("Тема 4. Геометрическое и аксиоматическое определения вероятности",""));
-                modelRcyclers.add(new ModelRcycler("Тема 5. Теоремы сложения и умножения вероятностей.",""));
-                modelRcyclers.add(new ModelRcycler("Тема 6. Формулы полной вероятности и Байеса",""));
-                modelRcyclers.add(new ModelRcycler("Тема 7. Последовательность независимых испытаний. ",""));
-                modelRcyclers.add(new ModelRcycler("Тема 8. Локальная и интегральная теоремы Лапласа",""));
-                modelRcyclers.add(new ModelRcycler("Тема 9. Случайные величины. Понятие случайной величины. Виды случайных величин.",""));
-                modelRcyclers.add(new ModelRcycler("Тема 10. Дискретные случайные величины. Закон распределения. Виды дискретных распределений",""));
-                modelRcyclers.add(new ModelRcycler("Тема 11-12. Числовые характеристики дискретных случайных величин и их свойства",""));
-                modelRcyclers.add(new ModelRcycler("Тема 13-14. Функции распределения и плотности непрерывных случайных величин, их свойства",""));
-                modelRcyclers.add(new ModelRcycler("Тема 15-16. Числовые характеристики непрерывных случайных величин. Виды непрерывных распределений",""));
-                modelRcyclers.add(new ModelRcycler("Тема 17. Закон больших чисел и его практическое значение. Понятие о центральной предельной теореме",""));
+                modelRcyclers.add(new ModelRcycler("Тема 1. Основные понятия и определения.","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema1.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 2. События и операции над ними.","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema2.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 3. Понятие вероятности события, его классическое и статистическое определения.","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema3.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 4. Геометрическое и аксиоматическое определения вероятности","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema4.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 5. Теоремы сложения и умножения вероятностей.","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema5.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 6. Формулы полной вероятности и Байеса","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema6.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 7. Последовательность независимых испытаний. ","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema7.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 8. Локальная и интегральная теоремы Лапласа","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema8.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 9. Случайные величины. Понятие случайной величины. Виды случайных величин.","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema9.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 10. Дискретные случайные величины. Закон распределения. Виды дискретных распределений","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema10.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 11-12. Числовые характеристики дискретных случайных величин и их свойства","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema11_12.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 13-14. Функции распределения и плотности непрерывных случайных величин, их свойства","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema13_14.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 15-16. Числовые характеристики непрерывных случайных величин. Виды непрерывных распределений","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema15_16.docx"));
+                modelRcyclers.add(new ModelRcycler("Тема 17. Закон больших чисел и его практическое значение. Понятие о центральной предельной теореме","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/tema17.docx"));
 
             }break;
             case 2: {
 
                 textViewSelectedChapterName.setText("Практическая часть");
 
-                modelRcyclers.add(new ModelRcycler("1. Основные понятия теории вероятностей",""));
-                modelRcyclers.add(new ModelRcycler("2. Классические теоремы теории вероятностей",""));
-                modelRcyclers.add(new ModelRcycler("3. Повторные независимые испытания",""));
-                modelRcyclers.add(new ModelRcycler("4. Случайная величина. Закон распределения случайной величины. Функция распределения",""));
-                modelRcyclers.add(new ModelRcycler("5. Числовые характеристики дискретных случайных величин",""));
-                modelRcyclers.add(new ModelRcycler("6. Плотность распределения вероятностей и числовые характеристики непрерывных случайных величин",""));
-                modelRcyclers.add(new ModelRcycler("7. Основные законы распределения непрерывных случайных величин",""));
-                modelRcyclers.add(new ModelRcycler("8. Закон больших чисел",""));
+                modelRcyclers.add(new ModelRcycler("1. Основные понятия теории вероятностей","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy1.docx"));
+                modelRcyclers.add(new ModelRcycler("2. Классические теоремы теории вероятностей","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy2.docx"));
+                modelRcyclers.add(new ModelRcycler("3. Повторные независимые испытания","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy3.docx"));
+                modelRcyclers.add(new ModelRcycler("4. Случайная величина. Закон распределения случайной величины. Функция распределения","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy4.docx"));
+                modelRcyclers.add(new ModelRcycler("5. Числовые характеристики дискретных случайных величин","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy5.docx"));
+                modelRcyclers.add(new ModelRcycler("6. Плотность распределения вероятностей и числовые характеристики непрерывных случайных величин","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy6.docx"));
+                modelRcyclers.add(new ModelRcycler("7. Основные законы распределения непрерывных случайных величин","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy7.docx"));
+                modelRcyclers.add(new ModelRcycler("8. Закон больших чисел","https://github.com/asadbekakmedov2001/Ehtimol_dastur/raw/main/app/src/main/assets/amaliy8.docx"));
 
             }break;
         }
